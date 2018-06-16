0e6af71121c9ad32dcca68b0e9e6999212b19bea
jackottawa <tteng1992@gmail.com>
2018-06-16 16:52:52 -0400

    first commit

---
+++ resources/assets/js/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <div class="message" v-for="status in statuses">
+                    <div class="message-header">
+                        <p>
+                            {{ status.user.name }} said...
+                        </p>
+                        <p>
+                            {{ status.created_at | ago | capitalize}}
+                        </p>
+                    </div>
+
+                    <div class="message-body" v-text="status.body">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import moment from 'moment';
+    import Status from '../models/Status';
+
+    export default {
+
+        data() {
+          return {
+              statuses: []
+          }
+        },
+
+        filters: {
+            ago(date) {
+                return moment(date).fromNow();
+
+            },
+
+            capitalize(value) {
+                return value.toUpperCase();
+            }
+        },
+
+        created() {
+            Status.all(statuses => this.statuses = statuses);
+                // .then(response => this.statuses = response.data);
+        },
+
+        methods: {
+            // postedOn(status) {
+            //     return moment(status.created_at).fromNow();
+            //
+            // }
+        }
+    }
+</script>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+
+    <title>Master Page</title>
+</head>
+<body>
+
+    <div id="app">
+        @include ('layouts.header')
+
+        <section class="section">
+            <div class="container">
+                <router-view></router-view>
+            </div>
+        </section>
+
+
+    </div>
+
+<script src="/js/app.js"></script>
+</body>
+</html>
